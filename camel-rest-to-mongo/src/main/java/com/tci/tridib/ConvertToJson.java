56025ae5b3dae0b3e09abcbdc0b135f808889c1b
rantri <chakraborty.tridib@gmail.com>
2015-03-17 12:31:56 +0530

    jaxb implementation

--- camel-rest-to-mongo/src/main/java/com/tci/tridib/ConvertToJson.java
+++ camel-rest-to-mongo/src/main/java/com/tci/tridib/ConvertToJson.java
@@ -1,4 +1,6 @@
-package com.tci.tridib;
+package com.sapient.tridib;
+
+import com.tci.item.bnt.ContentCafe;
 
 public class ConvertToJson {
 	
@@ -9,6 +11,24 @@ public class ConvertToJson {
 		System.out.println("ConvertToJson.convertStringToJson() === "+str.toString());
 		System.out.println("Length === "+str.length);
 		return str;
+	}
+	
+	public String removeNameSpace(String str){
+		
+		//str.replace("<ContentCafe xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns=\"http://ContentCafe2.btol.com\"", "<ContentCafe ");
+		if(str.contains("<Product>true</Product>")){
+			return "true";
+		} else {
+			return "false";
+		}
+		//System.out.println("ConvertToJson.removeNameSpace() === "+str);		
+		
+	}
+	
+	public void checkProductAvailability(ContentCafe cc){
+		
+		System.out.println("ProductAvailable == "+cc.getRequestItems().getRequestItem().getAvailableContent().getProduct());
+		
 	}
 
 }
