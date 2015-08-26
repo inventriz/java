ecd528f5f7d049b3aa202f54df465c4e1104e324
rantri <chakraborty.tridib@gmail.com>
2015-08-27 02:40:32 +0530

    creating a greeting rest web service

--- spring-boot-web/src/main/java/com/tridib/inventriz/boot/controller/HelloController.java
+++ spring-boot-web/src/main/java/com/tridib/inventriz/boot/controller/HelloController.java
@@ -1,13 +1,27 @@
 package com.tridib.inventriz.boot.controller;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tridib.inventriz.boot.model.Greeting;
 
 @RestController
 @EnableAutoConfiguration
 public class HelloController {
+	
+	private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
 	
 	@RequestMapping("/")
 	String getHome(){
