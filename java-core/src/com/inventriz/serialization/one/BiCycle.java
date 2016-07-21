ebffb912df14e701b06d83750149c630f1cbd61b
rantri <chakraborty.tridib@gmail.com>
2016-07-22 00:56:17 +0530

    serialize and deserialize with object output stream

---
+++ java-core/src/com/inventriz/serialization/one/BiCycle.java
@@ -0,0 +1,106 @@
+package com.inventriz.serialization.one;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class BiCycle implements Serializable{
+	
+	private String color;
+	private String length;
+	private Date manufactureDate;
+	private int noOfGear;
+	private boolean hasSitCover;
+	
+	//public BiCycle(){}
+	
+	public BiCycle(String color, String length, Date manufactureDate,
+			int noOfGear, boolean hasSitCover) {
+		super();
+		this.color = color;
+		this.length = length;
+		this.manufactureDate = manufactureDate;
+		this.noOfGear = noOfGear;
+		this.hasSitCover = hasSitCover;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getLength() {
+		return length;
+	}
+	public void setLength(String length) {
+		this.length = length;
+	}
+	public Date getManufactureDate() {
+		return manufactureDate;
+	}
+	public void setManufactureDate(Date manufactureDate) {
+		this.manufactureDate = manufactureDate;
+	}
+	public int getNoOfGear() {
+		return noOfGear;
+	}
+	public void setNoOfGear(int noOfGear) {
+		this.noOfGear = noOfGear;
+	}
+	public boolean isHasSitCover() {
+		return hasSitCover;
+	}
+	public void setHasSitCover(boolean hasSitCover) {
+		this.hasSitCover = hasSitCover;
+	}
+	@Override
+	public String toString() {
+		return "BiCycle [color=" + color + ", length=" + length
+				+ ", manufactureDate=" + manufactureDate + ", noOfGear="
+				+ noOfGear + ", hasSitCover=" + hasSitCover + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((color == null) ? 0 : color.hashCode());
+		result = prime * result + (hasSitCover ? 1231 : 1237);
+		result = prime * result + ((length == null) ? 0 : length.hashCode());
+		result = prime * result
+				+ ((manufactureDate == null) ? 0 : manufactureDate.hashCode());
+		result = prime * result + noOfGear;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BiCycle other = (BiCycle) obj;
+		if (color == null) {
+			if (other.color != null)
+				return false;
+		} else if (!color.equals(other.color))
+			return false;
+		if (hasSitCover != other.hasSitCover)
+			return false;
+		if (length == null) {
+			if (other.length != null)
+				return false;
+		} else if (!length.equals(other.length))
+			return false;
+		if (manufactureDate == null) {
+			if (other.manufactureDate != null)
+				return false;
+		} else if (!manufactureDate.equals(other.manufactureDate))
+			return false;
+		if (noOfGear != other.noOfGear)
+			return false;
+		return true;
+	}
+	
+	
+
+}
---
+++ java-core/src/com/inventriz/serialization/one/TestSerialization.java
@@ -0,0 +1,43 @@
+package com.inventriz.serialization.one;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Date;
+
+public class TestSerialization {
+	
+	String path="D:\\path\\to\\write\\";
+	
+	void serialize(String file, Object obj) throws IOException{
+		FileOutputStream fos = new FileOutputStream(path.concat(file));
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		oos.writeObject(obj);		
+		oos.close();
+	}
+	
+	Object deserialize(String file) throws IOException, ClassNotFoundException{		
+		FileInputStream fis = new FileInputStream(path.concat(file));
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		Object obj = ois.readObject();
+		ois.close();
+		return obj;
+	}
+	
+	public static void main(String args[]){
+		TestSerialization ts = new TestSerialization();
+		BiCycle biCycle = new BiCycle("blue","20", new Date(), 4, true);
+		try {
+			ts.serialize("bicycle.txt", biCycle);
+			
+			BiCycle deBc = (BiCycle)ts.deserialize("bicycle.txt");
+			System.out.println(deBc.toString());
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
