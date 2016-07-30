c9430a161852a9c1ad12f150cbc47de957d27b82
rantri <chakraborty.tridib@gmail.com>
2016-07-30 20:48:54 +0530

    Test class to check method and field reflection

---
+++ invent-json/src/com/inventriz/json/test/Car.java
@@ -0,0 +1,113 @@
+package com.inventriz.json.test;
+
+public class Car {
+	
+	private String make;
+	private String model;
+	private String transmissionType;
+	private int year;
+	private int id;
+	private boolean isHacthBack;
+	private long kmDriven;
+	private double length;
+	private double height;
+	private String color;
+	private int noOfPersonTravel;
+	
+		
+	public Car() {
+		super();
+	}
+	public Car(String make, String model, String transmissionType, int year) {
+		super();
+		this.make = make;
+		this.model = model;
+		this.transmissionType = transmissionType;
+		this.year = year;
+	}
+	public Car(String make, String model, String transmissionType, int year,
+			int id, boolean isHacthBack, long kmDriven, double length,
+			double height, String color, int noOfPersonTravel) {
+		super();
+		this.make = make;
+		this.model = model;
+		this.transmissionType = transmissionType;
+		this.year = year;
+		this.id = id;
+		this.isHacthBack = isHacthBack;
+		this.kmDriven = kmDriven;
+		this.length = length;
+		this.height = height;
+		this.color = color;
+		this.noOfPersonTravel = noOfPersonTravel;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String getTransmissionType() {
+		return transmissionType;
+	}
+	public void setTransmissionType(String transmissionType) {
+		this.transmissionType = transmissionType;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public String getMake() {
+		return make;
+	}
+	public void setMake(String make) {
+		this.make = make;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public boolean isHacthBack() {
+		return isHacthBack;
+	}
+	public void setHacthBack(boolean isHacthBack) {
+		this.isHacthBack = isHacthBack;
+	}
+	public long getKmDriven() {
+		return kmDriven;
+	}
+	public void setKmDriven(long kmDriven) {
+		this.kmDriven = kmDriven;
+	}
+	public double getLength() {
+		return length;
+	}
+	public void setLength(double length) {
+		this.length = length;
+	}
+	public double getHeight() {
+		return height;
+	}
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public int getNoOfPersonTravel() {
+		return noOfPersonTravel;
+	}
+	public void setNoOfPersonTravel(int noOfPersonTravel) {
+		this.noOfPersonTravel = noOfPersonTravel;
+	}
+	
+	
+
+}
---
+++ invent-json/src/com/inventriz/json/test/TestInventJson.java
@@ -0,0 +1,69 @@
+package com.inventriz.json.test;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class TestInventJson {
+
+	public static void main(String arg[]) {
+		Car c = new Car("Hundai", "i10", "manual", 2012);
+		TestInventJson tj = new TestInventJson();
+
+		System.out.println(tj.makeJson(c));
+	}
+
+	String makeJson(Object obj) {
+
+		StringBuilder sb = null;
+		if (obj != null) {
+			sb = new StringBuilder("{");
+			Field[] fields = obj.getClass().getDeclaredFields();
+			for (Field f : fields) {
+				// System.out.println(f.getName());
+				sb.append("{");
+				sb.append("\"" + f.getName() + "\"");
+				sb.append(":");
+				
+				String fieldName = f.getName();
+				char c = fieldName.charAt(0);
+				fieldName = fieldName.replaceFirst(String.valueOf(c), String.valueOf(c).toUpperCase());
+				System.out.println("Method = get"+fieldName);
+				
+				sb.append(getValue(obj, "get"+fieldName));
+				sb.append("}");
+				sb.append(",");
+			}
+			sb.replace(sb.lastIndexOf(","), sb.lastIndexOf(",") + 1, "");
+			sb.append("}");
+		}
+
+		if (sb != null) {
+			return sb.toString();
+		}
+
+		return "{}";
+	}
+
+	Object getValue(Object obj, String str) {
+
+		Object o = null;
+		if (obj != null) {
+			try {
+
+				Method m = obj.getClass().getMethod(str);
+				o = m.invoke(obj);
+				System.out.println("from method = " + o);
+
+			} catch (SecurityException | IllegalArgumentException
+					| IllegalAccessException | NoSuchMethodException
+					| InvocationTargetException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return o;
+	}
+
+}
